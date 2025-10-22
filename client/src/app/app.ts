281725b294e9452bd226de065575b2a782328a9e
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-10-22 14:43:15 -0300

    Implementado página inicial

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { ShellComponent } from './components/shell/shell.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [ShellComponent, RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {}
---
+++ client/src/app/components/shell/shell.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.scss',
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    RouterLink,
+  ],
+})
+export class ShellComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => result.matches),
+    shareReplay(),
+  );
+
+  linksNavbar = [
+    { titulo: 'Inicio', icone: 'home', link: '/inicio' },
+    { titulo: 'Categorias', icone: 'apps', link: '/categorias' },
+    { titulo: 'Notas', icone: 'description', link: '/notas' },
+  ];
+}
